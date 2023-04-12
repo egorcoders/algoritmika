cb9c2291864cde82b8a88800a4f26e08c38d220f
Boev Egor <egorcoders@gmail.com>
2023-04-12 19:15:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-from direct.showbase.ShowBase import ShowBase
-from mapmanager import Mapmanager
+from turtle import *
+from random import randint
+tsize = 20
+s_width = 200
+s_height = 180
+class Sprite(Turtle):
+  def __init__(self, x, y, step=10, shape='circle', color='black'):
+      super().__init__()
+      self.penup()
+      self.speed(0)
+      self.goto(x, y)
+      self.color(color)
+      self.shape(shape)
+      self.step = step
+      self.points = 0
 
 
-class Game(ShowBase):
-   def __init__(self):
-       ShowBase.__init__(self)
-       self.land = Mapmanager()
-       self.land.loadLand("land.txt")
-       base.camLens.setFov(90)
+  def move_up(self):
+      self.goto(self.xcor(), self.ycor() + self.step)
+  def move_down(self):
+      self.goto(self.xcor(), self.ycor() - self.step)
+  def move_left(self):
+      self.goto(self.xcor() - self.step, self.ycor())
+  def move_right(self):
+      self.goto(self.xcor() + self.step, self.ycor())
 
 
-game = Game()
-game.run()
+player = Sprite(0, -100, 10, 'circle', 'orange')
+enemy1 = Sprite(-s_width, 0, 15, 'square', 'red')
+enemy2 = Sprite(s_width, 70, 15, 'square', 'red')
+goal = Sprite(0, 120, 20, 'triangle', 'green')
+
+
+scr = player.getscreen()
+scr.listen()
+scr.onkey(player.move_up, 'Up')
+scr.onkey(player.move_left, 'Left')
+scr.onkey(player.move_right, 'Right')
+scr.onkey(player.move_down, 'Down')
