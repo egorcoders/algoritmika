1365d0aded6870ffe98fef6fa9eb60daee7197e2
Boev Egor <egorcoders@gmail.com>
2023-05-17 19:31:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,109 +1,138 @@
-
-from pygame import *
-'''Необходимые классы'''
-
-
-#класс-родитель для спрайтов
-class GameSprite(sprite.Sprite):
-   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
-       super().__init__()
-       self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
-       self.speed = player_speed
-       self.rect = self.image.get_rect()
-       self.rect.x = player_x
-       self.rect.y = player_y
+import pygame
+import time
+pygame.init()
 
 
-   def reset(self):
-       window.blit(self.image, (self.rect.x, self.rect.y))
+'''создаём окно программы'''
 
 
-class Player(GameSprite):
-   def update_r(self):
-       keys = key.get_pressed()
-       if keys[K_UP] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_DOWN] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
-   def update_l(self):
-       keys = key.get_pressed()
-       if keys[K_w] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_s] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
-
-
-#игровая сцена:
 back = (200, 255, 255) #цвет фона (background)
-win_width = 600
-win_height = 500
-window = display.set_mode((win_width, win_height))
-window.fill(back)
+mw = pygame.display.set_mode((500, 500)) #окно программы (main window)
+mw.fill(back)
+clock = pygame.time.Clock()
 
 
-#флаги, отвечающие за состояние игры
-game = True
-finish = False
-clock = time.Clock()
-FPS = 60
+'''класс прямоугольник'''
 
 
-#создания мяча и ракетки   
-racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
-racket2 = Player('racket.png', 520, 200, 4, 50, 150)
-ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+class Area():
+   def __init__(self, x=0, y=0, width=10, height=10, color=None):
+       self.rect = pygame.Rect(x, y, width, height) #прямоугольник
+       self.fill_color = color
 
 
-font.init()
-font = font.Font(None, 35)
-lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
-lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+   def color(self, new_color):
+       self.fill_color = new_color
 
 
-speed_x = 3
-speed_y = 3
+   def fill(self):
+       pygame.draw.rect(mw, self.fill_color, self.rect)
+   def outline(self, frame_color, thickness): #обводка существующего прямоугольника
+       pygame.draw.rect(mw, frame_color, self.rect, thickness)    
 
 
-while game:
-   for e in event.get():
-       if e.type == QUIT:
-           game = False
-  
-   if finish != True:
-       window.fill(back)
-       racket1.update_l()
-       racket2.update_r()
-       ball.rect.x += speed_x
-       ball.rect.y += speed_y
+   def collidepoint(self, x, y):
+       return self.rect.collidepoint(x, y)       
 
 
-       if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
-           speed_x *= -1
-           speed_y *= 1
-      
-       #если мяч достигает границ экрана, меняем направление его движения
-       if ball.rect.y > win_height-50 or ball.rect.y < 0:
-           speed_y *= -1
+'''класс надпись'''
 
 
-       #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
-       if ball.rect.x < 0:
-           finish = True
-           window.blit(lose1, (200, 200))
-           game_over = True
+class Label(Area):
+   def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
+       self.image = pygame.font.SysFont('verdana', fsize).render(text, True, text_color)
 
 
-       #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
-       if ball.rect.x > win_width:
-           finish = True
-           window.blit(lose2, (200, 200))
-           game_over = True
+   def draw(self, shift_x=0, shift_y=0):
+       self.fill()
+       mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
 
 
-       racket1.reset()
-       racket2.reset()
-       ball.reset()
+RED = (255, 0, 0)
+GREEN = (0, 255, 51)
+YELLOW = (255, 255, 0)
+DARK_BLUE = (0, 0, 100)
+BLUE = (80, 80, 255)
 
 
-   display.update()
-   clock.tick(FPS)
+cards = []
+num_cards = 4
+
+
+x = 70
+
+
+for i in range(num_cards):
+   new_card = Label(x, 170, 70, 100, YELLOW)
+   new_card.outline(BLUE, 10)
+   new_card.set_text('CLICK', 26)
+   cards.append(new_card)
+   x = x + 100
+
+
+
+
+'''Бонус: заготовки для счётчиков времени и очков
+start_time = time.time()
+old_time = time.time()
+
+
+time_text = Label(0,0,50,50,back)
+time_text.set_text('Время:',40, DARK_BLUE)
+time_text.draw(20, 20)
+
+
+score_text = Label(380,0,50,50,back)
+score_text.set_text('Счёт:',45, DARK_BLUE)
+score_text.draw(20,20)
+
+
+timer = Label(50,55,50,40,back)
+timer.set_text('0', 40, DARK_BLUE)
+timer.draw(0,0)
+
+
+score = Label(430,55,50,40,back)
+score.set_text('0', 40, DARK_BLUE)
+score.draw(0,0)'''
+
+
+wait = 0
+
+
+from random import randint
+
+
+while True:
+   if wait == 0:
+       #переносим надпись:
+       wait = 20 #столько тиков надпись будет на одном месте
+       click = randint(1, num_cards)
+       for i in range(num_cards):
+           cards[i].color(YELLOW)
+           if (i + 1) == click:
+               cards[i].draw(10, 40)
+           else:
+               cards[i].fill()
+   else:
+       wait -= 1
+
+
+   #на каждом тике проверяем клик:
+   for event in pygame.event.get():
+       if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+           x, y = event.pos
+           for i in range(num_cards):
+               #ищем, в какую карту попал клик
+               if cards[i].collidepoint(x,y):
+                  if i + 1 == click: #если на карте есть надпись - перекрашиваем в зелёный, плюс очко
+                       cards[i].color(GREEN)
+                   else: #иначе перекрашиваем в красный, минус очко
+                       cards[i].color(RED)
+
+
+                   cards[i].fill()
+
+
+   pygame.display.update()
+   clock.tick(40)
