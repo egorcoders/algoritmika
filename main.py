aa6fdfc18eb36bacb0af93bb8d503c10da90f159
Boev Egor <egorcoders@gmail.com>
2023-05-31 19:33:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import pygame
 
 pygame.init()
 
-'''создаём окно программы'''
-
 back = (200, 255, 255)  # цвет фона (background)
 mw = pygame.display.set_mode((500, 500))  # окно программы (main window)
 mw.fill(back)
 clock = pygame.time.Clock()
 
-'''класс прямоугольник'''
+# переменные, отвечающие за координаты платформы
+racket_x = 200
+racket_y = 330
+
+# флаг окончания игры
+game_over = False
 
 
+# класс из предыдущего проекта
 class Area():
     def __init__(self, x=0, y=0, width=10, height=10, color=None):
-        self.rect = pygame.Rect(x, y, width, height)  # прямоугольник
-        self.fill_color = color
+        self.rect = pygame.Rect(x, y, width, height)
+        self.fill_color = back
+        if color:
+            self.fill_color = color
 
     def color(self, new_color):
         self.fill_color = new_color
@@ -23,41 +29,56 @@ class Area():
     def fill(self):
         pygame.draw.rect(mw, self.fill_color, self.rect)
 
-    def outline(self, frame_color, thickness):  # обводка существующего прямоугольника
-        pygame.draw.rect(mw, frame_color, self.rect, thickness)
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)
+
+    def colliderect(self, rect):
+        return self.rect.colliderect(rect)
 
 
-'''класс надпись'''
+# класс для объектов-картинок
+class Picture(Area):
+    def __init__(self, filename, x=0, y=0, width=10, height=10):
+        Area.__init__(self, x=x, y=y, width=width, height=height, color=None)
+        self.image = pygame.image.load(filename)
+
+    def draw(self):
+        mw.blit(self.image, (self.rect.x, self.rect.y))
 
 
-class Label(Area):
-    def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
-        self.image = pygame.font.SysFont('verdana', fsize).render(text, True, text_color)
+# создание мяча и платформы   
+ball = Picture('ball.png', 160, 200, 50, 50)
+platform = Picture('platform.png', racket_x, racket_y, 100, 30)
 
-    def draw(self, shift_x=0, shift_y=0):
-        self.fill()
-        mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+# создание врагов
+start_x = 5  # координаты создания первого монстра
+start_y = 5
+count = 9  # количество монстров в верхнем ряду
+monsters = []  # список для хранения объектов-монстров
+for j in range(3):  # цикл по столбцам
+    y = start_y + (55 * j)  # координата монстра в каждом след. столбце будет смещена на 55 пикселей по y
+    x = start_x + (27.5 * j)  # и 27.5 по x
+    for i in range(count):  # цикл по рядам (строкам) создаёт в строке количество монстров, равное count
+        d = Picture('enemy.png', x, y, 50, 50)  # создаём монстра
+        monsters.append(d)  # добавляем в список
+        x = x + 55  # увеличиваем координату следующего монстра
+    count = count - 1  # для следующего ряда уменьшаем кол-во монстров
 
+while not game_over:
+    ball.fill()
+    platform.fill()
 
-YELLOW = (255, 255, 0)
-DARK_BLUE = (0, 0, 100)
-BLUE = (80, 80, 255)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
 
-cards = []
-num_cards = 4
+    # отрисовываем всех монстров из списка
+    for m in monsters:
+        m.draw()
 
-x = 70
-
-for i in range(num_cards):
-    new_card = Label(x, 170, 70, 100, YELLOW)
-    new_card.outline(BLUE, 10)
-    new_card.set_text('CLICK', 26)
-    cards.append(new_card)
-    x = x + 100
-
-while True:
-    for card in cards:
-        card.draw(10, 30)
+    platform.draw()
+    ball.draw()
 
     pygame.display.update()
+
     clock.tick(40)
